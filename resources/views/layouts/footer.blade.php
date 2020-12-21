cf4206447904e65d459eb90fe70b76329b601e69
manojbhatta <jmsbhatta@gmail.com>
2020-12-21 11:01:21 +0545

    wip

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,10 +2,11 @@
 <footer class="page-footer font-small special-color-dark">
 	<div class="footer-copyright d-flex p-4">
 		<div class="">
-			© {{ now()->format('Y') }} Copyright <a href="http://digitalpaschim.com">Digital Paschim </a>
+			© {{ now()->format('Y') }} Copyright <a href="{{ url('/') }}">{{ config('app.name')}} </a>
 		</div>
 		<div class="ml-auto">
-			Developed By <a href="http://manojbhatta.com.np">Manoj Bhatta</a> at <a href="http://mohrain.com">Mohrain WebSoft Pvt. Ltd.</a>
+			{{-- Developer <a href="http://manojbhatta.com.np">Manoj Bhatta</a> --}}
+			Powered By <a href="http://mohrain.com">Mohrain WebSoft Pvt. Ltd.</a>
 		</div>
 	</div>
 </footer>
